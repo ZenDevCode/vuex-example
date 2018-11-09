c06d0705550f384a49c5958ba5b58b99d9e91f8f
kball <kball@zendev.com>
2018-11-09 15:38:26 -0800

    Add sample store

--- src/store.js
+++ src/store.js
@@ -5,12 +5,21 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    pageviews: 0,
+    clicks: 0,
+  },
+  getters: {
+    actions: (state) => {
+      return state.pageviews + state.clicks;
+    }
   },
   mutations: {
-
+    addPageview(state) {
+      state.pageviews += 1;
+    },
+    addClick(state) {
+      state.clicks += 1;
+    } 
   },
-  actions: {
-
-  }
 })
+
