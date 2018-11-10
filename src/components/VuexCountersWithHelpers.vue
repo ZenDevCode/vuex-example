4512a9b937cb8608c60c212df7c395c987c06bff
kball <kball@zendev.com>
2018-11-10 10:53:33 -0800

    VuexCountersWithHelpers

---
+++ src/components/VuexCountersWithHelpers.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <h3>This component DOES use Vuex</h3>
+    <table>
+      <tr>
+        <th>
+          PageViews
+        </th>
+        <th>
+          Clicks
+        </th>
+        <th>
+          Total Actions
+        </th>
+      </tr>
+      <tr>
+        {{pageviews}}
+      </tr>
+      <tr>
+        {{clicks}}
+      </tr>
+      <tr>
+        {{actions}}
+      </tr>
+    </table>
+    <button @click="incrementClicks">Click Me</button>
+  </div>
+</template>
+<script>
+import { mapState, mapGetters, mapMutations } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState({
+      pageviews: state => state.pageviews,
+      clicks: state => state.clicks,
+    }),
+    ...mapGetters({
+      actions: 'actions'
+    })
+  },
+  methods: {
+    ...mapMutations({
+      incrementClicks: 'addClick',
+    }),
+  },
+  mounted() {
+    this.$store.commit('addPageview')
+  }
+};
+</script>
+
